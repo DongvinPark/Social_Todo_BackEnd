249864cedbce60d21280153729f52311f9fd25e3
Dongvin,Park <dongvin99@naver.com>
2022-12-13 16:11:41 +0900

    initial commit

---
+++ src/main/java/com/example/socialtodobackend/SocialTodoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialtodobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialTodoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialTodoBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/socialtodobackend/SocialTodoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialtodobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialTodoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

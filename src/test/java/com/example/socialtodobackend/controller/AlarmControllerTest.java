9638b9a5e152afd8f8f4f2a0c5415615c15729df
Dongvin,Park <dongvin99@naver.com>
2023-01-01 18:55:13 +0900

    Follow And Follow Related Alarm Test Code Added

---
+++ src/test/java/com/example/socialtodobackend/controller/AlarmControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.socialtodobackend.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlarmControllerTest {
+
+}
---
+++ src/test/java/com/example/socialtodobackend/service/AlarmServiceTest.java
@@ -0,0 +1,7 @@
+package com.example.socialtodobackend.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlarmServiceTest {
+
+}

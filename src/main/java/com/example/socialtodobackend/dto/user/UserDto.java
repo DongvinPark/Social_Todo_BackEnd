5ac0b2e9021969dcce3a83919153db271a719004
Dongvin,Park <dongvin99@naver.com>
2023-02-18 17:15:01 +0900

    유저 정보 응답용 Dto에 email 정보 추가

--- src/main/java/com/example/socialtodobackend/dto/user/UserDto.java
+++ src/main/java/com/example/socialtodobackend/dto/user/UserDto.java
@@ -25,17 +25,11 @@ public class UserDto {
 
     private LocalDateTime registeredAt;
 
-
-
-    /**
-     * 백엔드에서 유저 정보를 프런트엔드로 넘길 때는 이메일 주소를 넘기지 말아야 한다.
-     * <br><br/>
-     * 이메일 주소는 로그인 시 사용되는  개인정보이기 때문에 누구에게나 공개되어도 괜찮은 정보는 아니다.
-     * */
     public static UserDto fromEntity(UserEntity userEntity){
         return UserDto.builder()
             .id(userEntity.getId())
             .nickname(userEntity.getNickname())
+            .emailAddr(userEntity.getEmailAddr())
             .statusMessage(userEntity.getStatusMessage())
             .registeredAt(userEntity.getRegisteredAt())
             .build();
